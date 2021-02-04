0e184eb30171cd6305667f41877ffee691704b2f
Aleksei Sidorin <sidorinaleksei@gmail.com>
2021-02-05 02:17:34 +0300

    add email sender

--- src/main/java/ru/sidorin/java_messager/controller/RegistrationController.java
+++ src/main/java/ru/sidorin/java_messager/controller/RegistrationController.java
@@ -2,20 +2,20 @@ package ru.sidorin.java_messager.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import ru.sidorin.java_messager.domain.Role;
 import ru.sidorin.java_messager.domain.User;
-import ru.sidorin.java_messager.repository.UserRepository;
+import ru.sidorin.java_messager.service.UserService;
 
-import java.util.Collections;
 import java.util.Map;
 
 @Controller
 public class RegistrationController {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
 
 
     @GetMapping("/registration")
@@ -25,17 +25,25 @@ public class RegistrationController {
 
     @PostMapping("/registration")
     public String addUser(User user, Map<String, Object> model) {
-        User userFromDb = userRepository.findByUsername(user.getUsername());
-
-        if (userFromDb != null) {
+        if (!userService.addUser(user)) {
             model.put("message", "User exists!");
             return "registration";
         }
 
-        user.setActive(true);
-        user.setRoles(Collections.singleton(Role.USER));
-        userRepository.save(user);
-
         return "redirect:/login";
     }
+
+    @GetMapping("/activate/{code}")
+    public String activate(Model model, @PathVariable String code) {
+        boolean isActivated = userService.activateUser(code);
+
+        if (isActivated) {
+            model.addAttribute("message", "User successfully activated");
+        } else {
+            model.addAttribute("message", "Activation code is not found!");
+        }
+
+        return "login";
+    }
+
 }
--- src/main/java/ru/sidorin/java_messager/domain/User.java
+++ src/main/java/ru/sidorin/java_messager/domain/User.java
@@ -34,6 +34,26 @@ public class User implements UserDetails {
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
 
+    private String email;
+    private String activationCode;
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getActivationCode() {
+        return activationCode;
+    }
+
+    public void setActivationCode(String activationCode) {
+        this.activationCode = activationCode;
+    }
+
     public boolean isAdmin(){
         return roles.contains(Role.ADMIN);
     }
